b60ed23f627803f68bc1173d86e1b3ccbff61134
Luciano Gabutti <luciano@cuadernorojo.com>
2023-06-02 15:35:31 -0300

    Uploading PR corrections

--- src/screens/home/components/subreddits/Subreddit.jsx
+++ src/screens/home/components/subreddits/Subreddit.jsx
@@ -5,10 +5,7 @@ const Subreddit = (props) => {
 
     const {title, icon_img} = props.subreddit
 
-    const handleOnClick = () => {
-        props.onClick(title.toLowerCase().replace(' ','_'))
-    }
-
+    const handleOnClick = () => props.onClick(title.toLowerCase().replace(' ','_'));
     return (
         <div className="subreddits" onClick={handleOnClick}>
             <div className="subreddit-container">
